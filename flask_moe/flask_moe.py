066e16bf14f116b905b4d331dffa6ca61ec6b318
JIHO LEE <search5@gmail.com>
2016-04-24 20:25:29 +0900

    site making..

--- flask_moe/flask_moe.py
+++ flask_moe/flask_moe.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return render_template('index.html')
+    return render_template('index.html', page="home")
 
 @app.route("/windows")
 def env_windows():
